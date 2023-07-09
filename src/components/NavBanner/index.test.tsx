cbffc8ebcc560da42dd7d8cafdf7e2a8b6a05bdb
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-07-08 21:01:46 -0300

    test: created unit test for nav banner component

---
+++ src/components/NavBanner/index.test.tsx
@@ -0,0 +1,45 @@
+import userEvent from '@testing-library/user-event'
+import { NavBanner } from '.'
+import { render, screen } from '@testing-library/react'
+import React, { Dispatch, useState } from 'react'
+
+const setOrderFn = jest.fn()
+const setFilterFn = jest.fn()
+const setState: Dispatch<unknown> = jest.fn()
+
+describe('Component > NavBanner', () => {
+  it('should be view element on NavBanner component', () => {
+    const { container } = render(
+      <NavBanner setOrder={setOrderFn} setFilter={setFilterFn} />,
+    )
+
+    expect(container.querySelectorAll('button')).toHaveLength(5)
+    expect(screen.getByTestId('select-ordered')).toBeInTheDocument()
+  })
+
+  it('should be click filter on NavBanner component (selected)', async () => {
+    jest.spyOn(React, 'useState').mockReturnValue(['Agências', setState])
+
+    render(<NavBanner setOrder={setOrderFn} setFilter={setFilterFn} />)
+
+    const button = screen.getByText('Agências')
+
+    await userEvent.click(button)
+  })
+
+  it('should be click filter on NavBanner component (unselected)', async () => {
+    jest.spyOn(React, 'useState').mockReturnValue(['Diferent', setState])
+
+    render(<NavBanner setOrder={setOrderFn} setFilter={setFilterFn} />)
+
+    const button = screen.getByText('Agências')
+
+    await userEvent.click(button)
+  })
+
+  it('should be click filter on NavBanner component (date)', async () => {
+    jest.spyOn(React, 'useState').mockReturnValue(['date', useState])
+
+    render(<NavBanner setOrder={setOrderFn} setFilter={setFilterFn} />)
+  })
+})
